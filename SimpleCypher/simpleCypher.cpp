a2086303ef2bc4d186661ecc86bdccb5da4c554d
Matthew Wiewel <mjvwiewel@gmail.com>
2021-09-14 20:05:45 -0500

    removed clunky case switch

--- SimpleCypher/simpleCypher.cpp
+++ SimpleCypher/simpleCypher.cpp
@@ -26,61 +26,19 @@ int main()
 //TODO: switch this from cumbersome switch case and use atoi
 string letterToNumber(char c)
 {
-	switch(c)
+	
+	//it's a letter: let's encode it
+	if(isalpha(c))
 	{
-		case 'a':
-			return "1";
-		case 'b':
-			return "2";
-		case 'c':
-			return "3";
-		case 'd':
-			return "4";
-		case 'e':
-			return "5";
-		case 'f':
-			return "6";
-		case 'g':
-			return "7";
-		case 'h':
-			return "8";
-		case 'i':
-			return "9";
-		case 'j':
-			return "10";
-		case 'k':
-			return "11";
-		case 'l':
-			return "12";
-		case 'm':
-			return "13";
-		case 'n':
-			return "14";
-		case 'o':
-			return "15";
-		case 'p':
-			return "16";
-		case 'q':
-			return "17";
-		case 'r':
-			return "18";
-		case 's':
-			return "19";
-		case 't':
-			return "20";
-		case 'u':
-			return "21";
-		case 'v':
-			return "22";
-		case 'w':
-			return "23";
-		case 'x':
-			return "24";
-		case 'y':
-			return "25";
-		case 'z':
-			return "26";
-		default:
-			return " ";
+		int asciiValue = int(c);
+		
+		if(asciiValue > 90)
+			return to_string(asciiValue - 96); //lower-case letter
+		else
+			return to_string(asciiValue - 64); //upper-case letter
 	}
+	
+	//not a letter: leave unchanged
+	string leaveNonLetterUnchanged{c};
+	return leaveNonLetterUnchanged;
 }
