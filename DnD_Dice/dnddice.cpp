d69850b82b4b5a3aeeaa1155b770e6e2c53d1baf
Matthew Wiewel <mjvwiewel@gmail.com>
2022-03-26 20:55:02 -0500

    program for rolling dice

---
+++ DnD_Dice/dnddice.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void get_num_above_zero(int& num, const char* prompt);
+
+int main()
+{
+	char again = 'y';
+	int rolls;
+	int sides;
+	int roll;
+	srand(time(0));
+	
+	while(again == 'y' || again == 'Y')
+	{
+		cout << "\nEnter number of sides to roll 1 die.";
+		cout << "\nEnter 0 to make cutsom with multiple rolls.";
+		cout << "\nInput: ";
+		cin >> sides;
+		
+		if(sides > 0)
+		{
+			roll = (rand() % sides) + 1;
+			cout << "\nRolling 1 d" << sides << ": " << roll;
+		}
+		else
+		{
+			get_num_above_zero(rolls, "\nEnter number of rolls: ");
+			get_num_above_zero(sides, "\nEnter number of sides: ");
+			
+			cout << "\nRolling d" << sides << " for " << rolls << " rolls.\n";
+			for(int i = 0; i < rolls; i++)
+			{
+				roll = (rand() % sides) + 1;
+				cout << roll << " ";
+			}
+			
+		}
+		
+		cout << "\nRoll another die? (Y/N): ";
+		cin >> again;
+	}
+	
+	return 0;
+}
+
+void get_num_above_zero(int& num, const char* prompt)
+{
+	do
+	{
+		cout << prompt;
+		cin >> num;
+		
+		if(num < 1)
+			cout << "\nError! Enter a number above 0!";
+		
+	}while(num < 1);
+}
