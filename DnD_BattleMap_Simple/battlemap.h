0c5b6dcffd680caeab3f7588ebd1fd0b38727f93
Matthew Wiewel <mjvwiewel@gmail.com>
2023-11-16 14:50:42 -0600

    can't remember what changes were made, but remember dropping the project

--- DnD_BattleMap_Simple/battlemap.h
+++ DnD_BattleMap_Simple/battlemap.h
@@ -4,22 +4,32 @@
 #include <string>
 #include <iostream>
 
+const char IMPASSABLE = 'x';
+const char ROUGH = 'r';
+const char OPEN = '-';
+
 class battlemap
 {
 private: 
 	struct square
 	{
 		char type;
-		bool occupied;
+		bool is_occupied;
 		std::string occupant;
 		
-		bool is_occupied() { return occupied; }
-		bool is_impassable() { return type == 'x'; }
+		bool is_impassable() { return type == IMPASSABLE; }
 	};
 	
 	int width;
 	int height;
 	square** arr; //TODO: figure out what the array holds, maybe use vector instead\
+	
+	~battlemap()
+	{
+		for(int i = 0; i < height; i++)
+			delete[] arr[i];
+		delete[] arr;
+	}
 	
 public:
 	void placeholder();
